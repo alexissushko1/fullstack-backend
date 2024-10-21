bd500261b3f03a681786f79726532330ed802c14
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-21 14:50:01 -0600

    Add 404, & error-handling middleware, and app.listen method

--- server.js
+++ server.js
@@ -5,3 +5,19 @@ const PORT = 3000;
 
 app.use(require("morgan")("dev"));
 app.use(express.json());
+
+//404 middleware
+app.use((req, res, next) => {
+  next({ status: 404, message: "Endpoint not found" });
+});
+
+//Error-handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status ?? 500);
+  res.json(err.message ?? "Sorry, something went wrong on the server side");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port#: ${3000}...`);
+});
