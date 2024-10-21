dcc5d1b4965dfd38a4f1121c64f8d89ff5c4ba3d
Alexis Sushko <alexis.sushko@gmail.com>
2024-10-21 16:28:33 -0400

    Create a get route to get a department with a specific id and include all of the professors in that department

--- api/departments.js
+++ api/departments.js
@@ -1,7 +1,7 @@
 //Import express, create router, import authenticate and prisma
 const express = require("express");
 const router = express.Router();
-//const { authenticate } = require("./auth");
+const { authenticate } = require("./auth");
 const prisma = require("../prisma");
 
 //Create get route to get all departments, catch errors and pass to next error handler
@@ -21,3 +21,27 @@ router.get("/", async (req, res, next) => {
     next(e);
   }
 });
+
+//Create a get route to get the department with a specific id
+/**
+ * @route GET /:id
+ * @group departments
+ * @param {String} id
+ * @returns {Object} department
+ * @throws {Error} if issue getting that department
+ */
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const department = await prisma.department.findUniqueOrThrow({
+      where: { id: +id },
+      include: { professors: true },
+    });
+    res.json(department);
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = router;
