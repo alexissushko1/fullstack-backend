c18b32b10f912372a337908231905df4eb513ca3
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-21 15:05:04 -0600

    Add router middlware for all requests to check for user and JWT, add register route for new user

--- api/auth.js
+++ api/auth.js
@@ -9,3 +9,39 @@ function createToken(id) {
 }
 
 const prisma = require("../prisma");
+
+router.use(async (req, res, next) => {
+  // Grab token from headers only if it exists
+  const authHeader = req.headers.authorization;
+  // Slice off the first 7 characters (Bearer ), leaving the token
+  const token = authHeader?.slice(7); // "Bearer <token>"
+  // If there is no token move on to the next middleware
+  if (!token) return next();
+
+  // TODO: Find user with ID decrypted from the token and attach to the request
+  try {
+    // Decodes the id from the token, using the secret code in env
+    // Assigns the id to variable id
+    const { id } = jwt.verify(token, JWT_SECRET);
+    const user = await prisma.user.findUniqueOrThrow({
+      where: { id },
+    });
+    // Attach the found customer to the request object
+    req.user = user;
+    // Move to the next middleware
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/register", async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const user = await prisma.user.register(username, password);
+    const token = createToken(user.id);
+    res.status(201).json({ token });
+  } catch (error) {
+    next(error);
+  }
+});
