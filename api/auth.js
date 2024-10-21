e94c4c87aef42222210069a679c580e2c7066a86
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-21 14:58:42 -0600

    Add auth.js, import JWT_SECRET from env, import jsonwebtoken, & express router.  add createToken funciton

---
+++ api/auth.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
+
+function createToken(id) {
+  return jwt.sign({ id }, JWT_SECRET, { expiresIn: "1d" });
+}
